ed057bc618b9bc9bde1650800e005fe7f31fe772
DonghanYang <donghany0818@naver.com>
2024-11-19 18:19:26 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: 'navy', height: '100vh', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>포트폴리오 작업중...</h1>
     </div>
   );
 }
