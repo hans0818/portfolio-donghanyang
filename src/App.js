6d465fe423b58ea2caaaafbfc5bb59a7aac2fa33
DonghanYang <donghany0818@naver.com>
2024-11-19 18:28:14 +0900

    Updated changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App" style={{ backgroundColor: 'navy', height: '100vh', color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <h1>포트폴리오 작업중...</h1>
+      <h1>깃허브 확인용</h1>
     </div>
   );
 }
